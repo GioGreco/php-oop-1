72ddb24761dcc33fb21fb9946db2081adf7d2ae2
GioGreco <grecogiorgio.97@gmail.com>
2022-12-12 16:14:46 +0100

    get movies from DB-like

---
+++ Database/moviesDB.php
@@ -0,0 +1,24 @@
+<?php
+
+$moviesDB = [
+    [
+        'title' => 'Interstellar',
+        'originalTitle' => null,
+        'director' => 'Cristopher Nolan',
+        'genres' => ['fantascienza', 'avventura', 'drammatico'],
+        'year' => 2014,
+        'originalLanguage' => 'eng',
+        'rating' => 8.5
+    ],
+    [
+        'title' => 'Avatar',
+        'originalTitle' => 'Blue Aliens',
+        'director' => 'James Cameron',
+        'genres' => ['fantascienza', 'azione', 'avventura'],
+        'year' => 2009,
+        'originalLanguage' => 'eng',
+        'rating' => 7.5
+    ]
+]
+
+?>
--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 include_once __DIR__ . '/Models/Movie.php';
 include __DIR__ . '/partials/header.php';
 
-$movie1 = new Movie('Interstellar', 'Cristopher Nolan', ['fantascienza', 'avventura', 'drammatico'], 2014, 'eng', 8.5);
-$movie2 = new Movie('Avatar', 'James Cameron', ['fantascienza', 'azione', 'avventura'], 2009, 'eng', 8, 'Blue Aliens');
+include __DIR__ . '/Database/moviesDB.php';
 
-var_dump($movie1);
-var_dump($movie2);
+$movies = [];
+
+foreach($moviesDB as $movie){
+    $newMovie = new Movie($movie['title'], $movie['director'], $movie['genres'], $movie['year'], $movie['originalLanguage'], $movie['rating'], $movie['originalTitle']);
+    array_push($movies, $newMovie);
+}
+
+var_dump($movies);
+
+// $movie1 = new Movie('Interstellar', 'Cristopher Nolan', ['fantascienza', 'avventura', 'drammatico'], 2014, 'eng', 8.5);
+// $movie2 = new Movie('Avatar', 'James Cameron', ['fantascienza', 'azione', 'avventura'], 2009, 'eng', 8, 'Blue Aliens');
+
+// var_dump($movie1);
+// var_dump($movie2);
 ?>
 
 <div id="app">
